1dba5e585b2eaae9777aed70a786a567628e4762
jonasjso <jonasjso@stud.ntnu.no>
2018-05-30 14:28:14 +0200

    oppgave6 - new file

---
+++ twoFactorAuthentication/oppgave6-unittest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "./vendor/autoload.php";
+
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase {
+
+
+    public function setup() {
+    }
+
+    public function testCreateUser() {
+    }
+
+}
+
+?>
